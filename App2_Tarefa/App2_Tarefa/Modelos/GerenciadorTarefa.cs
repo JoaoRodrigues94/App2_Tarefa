5dd087e9fb9c549e0dee695e9d00ec75a3bf59b6
Joao Paulo <joaopaulo.fr.1994@gmail.com>
2021-07-10 21:58:24 -0300

    Adicionar arquivos de projeto.

---
+++ App2_Tarefa/App2_Tarefa/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2_Tarefa.Modelos
+{
+  public class GerenciadorTarefa
+  {
+    private List<Tarefa> Lista { get; set; }
+    public void Salvar(Tarefa tarefa)
+    {
+      Lista = Listagem();
+      Lista.Add(tarefa);
+
+      SalvarNoProperties(Lista);
+    }
+    public void Deletar(int index)
+    {
+      Lista = Listagem();
+      Lista.RemoveAt(index);
+
+      SalvarNoProperties(Lista);
+    }
+    public void Finalizar(int index, Tarefa tarefa)
+    {
+      Lista = Listagem();
+      Lista.RemoveAt(index);
+
+      tarefa.DataFinalizacao = DateTime.Now;
+      Lista.Add(tarefa);
+      SalvarNoProperties(Lista);
+    }
+    public List<Tarefa> Listagem()
+    {
+
+      return ListagemProperties();
+    }
+    private void SalvarNoProperties(List<Tarefa> Lista)
+    {
+      if (App.Current.Properties.ContainsKey("Tarefas"))
+      {
+        App.Current.Properties.Remove("Tarefas");
+      }
+      App.Current.Properties.Add("Tarefas", Lista);
+    }
+    private List<Tarefa> ListagemProperties()
+    {
+      if (App.Current.Properties.ContainsKey("Tarefas"))
+      {
+        return (List<Tarefa>)App.Current.Properties["Tarefas"];
+      }
+      return new List<Tarefa>();
+    }
+  }
+}
